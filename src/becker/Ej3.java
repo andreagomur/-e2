7e070862a36e18c30975045dca1c251080662c4b
andrecalderon <andrea_calderon@hotmail.com>
2017-08-09 12:25:28 -0500

    P1

--- src/becker/Ej3.java
+++ src/becker/Ej3.java
@@ -3,31 +3,39 @@ package becker;
 import becker.robots.*;
 
 public class Ej3 {
+   
+    public static void GiraDerecha(Robot robot) {
+              robot.turnLeft();
+              robot.turnLeft();
+              robot.turnLeft();
+    }
+    public static void Cambiasentido(Robot robot) {
+              robot.turnLeft();
+              robot.turnLeft();
+    }
 
     public static void main(String[] args) {
         City RJ = new City();
         Robot baco = new Robot(RJ, 1, 0, Direction.EAST, 5);
 
         int x = 6;
-        int y = 6;
+        int y = 5;
         for (int i = 1; i <= y; i = i + 1) {
             for (int j = 1; j <= x; j = j + 1) {
-                Thing t2 = new Thing(RJ, j, i);
+                Thing t1 = new Thing(RJ, j, i);
             }
         }
-
-        /*
-      baco.move();
-      baco.move();
-      baco.pickThing();
-      baco.move();
-      baco.turnLeft();	// start turning right as three turn lefts
-      baco.turnLeft();
-      baco.turnLeft();	// finished turning right
-      baco.move();
-     // ;
-      baco.move();
-         */
-        System.out.println(baco.countThingsInBackpack());
+                 for (int l = 1; l <=x; l = l + 1) {
+         for (int i = 1; i <=2; i = i + 1) {
+            for (int j = 1; j <= y; j = j + 1) {
+            baco.move();
+            baco.pickThing();
+            }
+            GiraDerecha (baco);
+                       }
+            y = y-1;
+                 }
+             GiraDerecha (baco);
+         System.out.println(baco.countThingsInBackpack());
+        }
     }
-}
